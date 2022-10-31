f9b78c3c654b55162ef4bfae0305bbb276490bda
renato3x <pereirarenato21@gmail.com>
2022-10-31 17:44:03 -0300

    rxjs parte 1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import path from 'path'
+import { Observable } from 'rxjs'
+import fs from 'fs'
 
 const filePaths: string[] = [
   path.join(__dirname, 'files', 'app_1.txt'),
@@ -17,3 +19,81 @@ const filePaths: string[] = [
 
 const isCSS = /^((.|#){0,1}(\w+-{0,1})+\s*{(\s*(\w+-{0,1})+:\s*(\w+\s*)+;\s*)+\s*}\s*)/i
 const isHTML = /^<!DOCTYPE html>/i
+
+function lerArquivos(arquivos: string[]) {
+  /*
+    Observables são fontes de dados que enviam dados/informações de forma contínua
+  */
+
+  /**
+   * A classe Observable recebe como parâmetro uma função responsável pela
+   * geração dos dados que o Observable enviará
+   */
+
+  /**
+   * subscriber é uma referência do dependente da informação 
+   */
+
+  const leitor = new Observable<string>((subscriber) => {
+    /**
+     * o método forEach (paraCada) Serve para fazer um laço de repetição
+     * dentro de um array
+     */
+
+    arquivos.forEach((arquivo) => {
+      /**
+       * readFileSync fará a leitura de um arquivo a partir do caminho desse
+       * arquivo no seu computador
+       */
+
+      const conteudo = fs.readFileSync(arquivo, { encoding: 'utf-8' })
+      subscriber.next(conteudo) // responsável por mandar a mensagem de sucesso
+      // subscriber.error() // responsável por mandar a mensagem de erro
+      // subscriber.complete() // responsável por mandar a mensagem de completo
+
+      /**
+       * --> Envio de Dados do Observable <--
+       * 
+       * 3 ESTÁGIOS
+       *   -> Sucesso: O Observable conseguiu realizar seu trabalho sem nenhum problema
+       *               e enviou os dados com sucesso
+       *   
+       *   -> Erro: O Observable teve algum problema durante a sua execução e não conseguiu
+       *            realizar sua tarefa de maneira satisfatória e não conseguiu enviar os dados
+       *            Quando um Observable passa pelo estágio de erro, sua execução para automaticamente
+       * 
+       *   -> Completo: O Observable realizou TODAS as suas tarefas com sucesso e não possui
+       *                mais nenhum dado para poder enviar.
+       */
+    })
+  })
+
+  return leitor
+}
+
+let obs = lerArquivos(filePaths)
+
+/**
+ * o método subscribe() dos observables te permite acessar os valores
+ * que o observable te envia de forma contínua
+ */
+
+/**
+ * 1° -> Sucesso
+ * 2° -> Erro
+ * 3° -> Completo
+ */
+
+obs.subscribe(
+  (conteudoLido) => {
+    console.log('---------- ARQUIVO LIDO COM SUCESSO ----------')
+    console.log(conteudoLido)
+    console.log('----------------------------------------------\n\n')
+  }
+)
+
+obs.subscribe(
+  (conteudoLido) => {
+    console.log(`Este arquivo possui ${conteudoLido.length} caracteres`)
+  }
+)
